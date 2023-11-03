8cfc4ab3098219b8e98749762cf18d85e8636b1e
Tyler Quach <59546424+TylerQuach1@users.noreply.github.com>
2023-11-03 12:38:06 -0400

    Successfull Connection to DB

--- example.js
+++ example.js
@@ -1,19 +1,26 @@
 const oracledb = require('oracledb');
 
-oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
+let clientOpts = {};
+if (process.platform === 'win32') {                                   // Windows
+    clientOpts = { libDir: 'C:\\oracle\\instantclient_21_12' };
+} else if (process.platform === 'darwin' && process.arch === 'x64') { // macOS Intel
+    clientOpts = { libDir: 'C:\\oracle\\instantclient_21_12' };
+} // else on other platforms the system library search path
+  // must always be set before Node.js is started.
+
+// enable Thick mode which is needed for SODA
+oracledb.initOracleClient(clientOpts);
 
 async function run() {
 
     const connection = await oracledb.getConnection ({
         user          : "",
         password      : "",
-        connectionString : "oracle.scs.ryerson.ca:1521/orcl"
+        connectString : "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl)))",
     });
 
     const result = await connection.execute(
-        `SELECT title
-         FROM movie`,
-        [103],  // bind value for :id
+        `SELECT * FROM movie`
     );
 
     console.log(result.rows);
